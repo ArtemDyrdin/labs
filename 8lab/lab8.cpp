511c08372e246dc2d134b9c513299c09a2ccc746
Artem Dyrdin <74466573+ArtemDyrdin@users.noreply.github.com>
2022-11-29 22:08:27 +0300

    add lab8
    working with pointers

---
+++ 8lab/lab8.cpp
@@ -0,0 +1,16 @@
+﻿// Дано целое число типа long long, выведите на экран содержимое каждого
+// из его байтов, используя знания по указателям.
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long a;
+    cin >> a;
+    cout << &a << endl;
+    unsigned char* y = (unsigned char*)&a;
+    for (int i = 0; i < 8; i++) {
+        cout << (int)*(y + i) << endl;
+    }
+}
